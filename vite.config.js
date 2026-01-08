6bd067b743497fe6cea55c20cb47034754b29970
Your Name <you@example.com>
2026-01-08 23:33:13 +0100

    third commit

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import { meta } from 'eslint-plugin-react-hooks'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: meta.env.VITE_BASE_PATH || "/success-kitchen",
+  base: meta.process.VITE_BASE_PATH || "/success-kitchen",
 })
