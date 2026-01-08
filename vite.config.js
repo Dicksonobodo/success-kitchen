1047909348424530136fcdbbeb760936fca2a9df
Your Name <you@example.com>
2026-01-08 23:41:40 +0100

    fouth commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { meta } from 'eslint-plugin-react-hooks'
+// import { meta } from 'eslint-plugin-react-hooks'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: meta.process.VITE_BASE_PATH || "/success-kitchen",
+  base: "/",
 })
