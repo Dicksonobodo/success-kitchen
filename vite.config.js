8a4d61e4cfee5e32051b87ab088384c27941dde1
Your Name <you@example.com>
2026-01-08 23:25:18 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { meta } from 'eslint-plugin-react-hooks'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/success-kitchen",
+  base: meta.env.VITE_BASE_PATH || "/success-kitchen",
 })
